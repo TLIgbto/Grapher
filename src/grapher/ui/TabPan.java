d75e281ddfb7066d9d18aa49e5372f81affc74d3
toure <toure@TOURE-VAIO>
2014-03-14 16:50:39 +0100

    The fucking fusion

---
+++ src/grapher/ui/TabPan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grapher.ui;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author toure
+ */
+public class TabPan extends JTable {
+
+    public TabPan(Grapher g, DefaultListModel<String> listModel) {
+
+    }
+}
---
+++ src/grapher/ui/TableModel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grapher.ui;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author toure
+ */
+public class TableModel extends AbstractTableModel {
+
+    @Override
+    public int getColumnCount() {
+        // TODO Auto-generated method stub
+        return 2;
+    }
+
+    @Override
+    public int getRowCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
